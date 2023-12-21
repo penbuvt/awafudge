810a61aa9cdf46eee8a0f4e34efcd67449e03a4d
Penbu <penbu@penbuvt.ca>
2023-12-20 20:10:39 -0500

    bf: Add tests for single instructions

--- test/brainfuck.js
+++ test/brainfuck.js
@@ -11,9 +11,63 @@ describe('brainfuck parser', () => {
     assert.strictEqual(actual, expected);
   });
 
+  it('parses a single right shift instruction', () => {
+    const input = '>';
+    const expected = 'awa~';
+
+    const actual = parse(input);
+
+    assert.strictEqual(actual, expected);
+  });
+
+  it('parses a single left shift instruction', () => {
+    const input = '<';
+    const expected = 'wa~';
+
+    const actual = parse(input);
+
+    assert.strictEqual(actual, expected);
+  });
+
   it('parses a single increment instruction', () => {
     const input = '+';
     const expected = 'awa';
+
+    const actual = parse(input);
+
+    assert.strictEqual(actual, expected);
+  });
+
+  it('parses a single decrement instruction', () => {
+    const input = '-';
+    const expected = 'wa';
+
+    const actual = parse(input);
+
+    assert.strictEqual(actual, expected);
+  });
+
+  it('parses a single write instruction', () => {
+    const input = '.';
+    const expected = '.';
+
+    const actual = parse(input);
+
+    assert.strictEqual(actual, expected);
+  });
+
+  it('parses a single read instruction', () => {
+    const input = ',';
+    const expected = ',';
+
+    const actual = parse(input);
+
+    assert.strictEqual(actual, expected);
+  });
+
+  it('parses a single empty loop instruction', () => {
+    const input = '[]';
+    const expected = '?!';
 
     const actual = parse(input);
 
