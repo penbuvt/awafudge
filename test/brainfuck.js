0bea493780ced2b114247f0bc93acd302a46f90c
Penbu <penbu@penbuvt.ca>
2023-12-20 20:13:54 -0500

    bf: Add test for ignoring non-instruction chars

--- test/brainfuck.js
+++ test/brainfuck.js
@@ -73,4 +73,13 @@ describe('brainfuck parser', () => {
 
     assert.strictEqual(actual, expected);
   });
+
+  it('ignores non-instruction characters', () => {
+    const input = '`1234567890=qwertyuiop\\asdfghjkl;\'zxcvbnm/ \n~!@#$%^&*()_QWERTYUIOP{}|ASDFGHJKL:"ZXCVBNM?';
+    const expected = '';
+
+    const actual = parse(input);
+
+    assert.strictEqual(actual, expected);
+  });
 });
