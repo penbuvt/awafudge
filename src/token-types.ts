314c1a6391e389ed761760b67e840cbb2adf3446
Penbu <penbu@penbuvt.ca>
2024-09-05 20:31:18 -0400

    Convert TokenTypes to enum

--- src/token-types.ts
+++ src/token-types.ts
@@ -1,12 +1,11 @@
-const TokenType = {
-  RightShift: 'RIGHT_SHIFT',
-  LeftShift: 'LEFT_SHIFT',
-  Increment: 'INCREMENT',
-  Decrement: 'DECREMENT',
-  Write: 'WRITE',
-  Read: 'READ',
-  Loop: 'LOOP',
-};
+export enum TokenType {
+  RightShift = 'RIGHT_SHIFT',
+  LeftShift = 'LEFT_SHIFT',
+  Increment = 'INCREMENT',
+  Decrement = 'DECREMENT',
+  Write = 'WRITE',
+  Read = 'READ',
+  Loop = 'LOOP',
+}
 
 export default TokenType;
-export { TokenType };
