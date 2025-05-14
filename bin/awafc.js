e4d000d68a131156f3c59f522e85c5c43bdc33fb
Penbu <penbu@penbuvt.ca>
2025-05-14 18:07:16 -0400

    Add support for sending compiled output to file
    Add the --output/-o flag to send the compiled code to a file.

--- bin/awafc.js
+++ bin/awafc.js
@@ -1,12 +1,27 @@
 #!/usr/bin/env node
 
-const { readFileSync } = require('fs');
+const { readFileSync, writeFileSync } = require('fs');
 const { stdout } = require('process');
 const { EOL } = require('os');
+const path = require('path');
+const nopt = require('nopt');
 const { AwafudgeParser, BrainfuckFormatter } = require('..');
+
+const options = nopt({
+  'output': path,
+}, {
+  'o': ['--output'],
+});
+
+const outFile = options.output;
 
 const input = readFileSync(0).toString();
 const parser = new AwafudgeParser();
 const ast = parser.parse(input);
 const formatted = BrainfuckFormatter.format(ast);
-stdout.write(formatted + EOL);
+
+if (outFile) {
+  writeFileSync(outFile, formatted + EOL);
+} else {
+  stdout.write(formatted + EOL);
+}
