e5bfd333bb26163adcd999d1116ca41c815fbee4
Penbu <penbu@penbuvt.ca>
2025-05-14 18:37:01 -0400

    Support specifying input and output languages
    Default to awafudge for input and brainfuck for output.
    Infer the other language if not specified.
    
    Specifying the same language for both input and output can be useful
    for formatting.

--- bin/awafc.js
+++ bin/awafc.js
@@ -5,10 +5,17 @@ const { stdout } = require('process');
 const { EOL } = require('os');
 const path = require('path');
 const nopt = require('nopt');
-const { AwafudgeParser, BrainfuckFormatter } = require('..');
+const {
+  AwafudgeParser,
+  AwafudgeFormatter,
+  BrainfuckParser,
+  BrainfuckFormatter,
+} = require('..');
 
 const options = nopt({
   'output': path,
+  'to': ['awafudge', 'brainfuck'],
+  'from': ['awafudge', 'brainfuck'],
 }, {
   'o': ['--output'],
 });
@@ -17,8 +24,25 @@ const options = nopt({
 const inFile = options.argv.remain[0] || 0; // default to stdin if not given
 const outFile = options.output || 1; // default to stdout if not given
 
+const swap = {
+  awafudge: 'brainfuck',
+  brainfuck: 'awafudge',
+};
+const fromLang = options.from ?? swap[options.to] ?? 'awafudge';
+const toLang = options.to ?? swap[options.from] ?? 'brainfuck';
+
+const parsers = {
+  awafudge: AwafudgeParser,
+  brainfuck: BrainfuckParser,
+};
+
+const formatters = {
+  awafudge: AwafudgeFormatter,
+  brainfuck: BrainfuckFormatter,
+};
+
 const input = readFileSync(inFile).toString();
-const parser = new AwafudgeParser();
+const parser = new (parsers[fromLang])();
 const ast = parser.parse(input);
-const formatted = BrainfuckFormatter.format(ast);
+const formatted = (formatters[toLang]).format(ast);
 writeFileSync(outFile, formatted + EOL);
