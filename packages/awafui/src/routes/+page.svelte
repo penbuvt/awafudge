c4c15130ee70c5cfae1a487e113cbf071d23e457
Penbu <penbu@penbuvt.ca>
2025-08-20 21:24:44 -0400

    awafui: Add ability to translate code
    Very bare-bones UI for now.

--- packages/awafui/src/routes/+page.svelte
+++ packages/awafui/src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="typescript">
+	import {
+		AwafudgeParser,
+		AwafudgeFormatter,
+		BrainfuckParser,
+		BrainfuckFormatter,
+	} from "awafudge";
+
+	let input = $state('');
+	let output = $state('');
+
+	function transpileToBrainfuck() {
+		const parser = new AwafudgeParser();
+		const ast = parser.parse(input);
+		const formatted = BrainfuckFormatter.format(ast);
+		output = formatted;
+	}
+
+	function transpileToAwafudge() {
+		const parser = new BrainfuckParser();
+		const ast = parser.parse(input);
+		const formatted = AwafudgeFormatter.format(ast);
+		output = formatted;
+	}
+</script>
+
+<h1>awafudge</h1>
+
+<button onclick={transpileToBrainfuck}>To brainfuck</button>
+<button onclick={transpileToAwafudge}>To awafudge</button>
+<textarea bind:value={input}></textarea>
+<textarea bind:value={output}></textarea>
