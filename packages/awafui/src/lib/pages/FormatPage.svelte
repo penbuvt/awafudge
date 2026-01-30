e344f9849529a1c4f6ef154962513159c83a437c
Penbu <penbu@penbuvt.ca>
2026-01-29 22:37:07 -0500

    awafui: Add Format page
    This page pretty-prints awafudge input.

---
+++ packages/awafui/src/lib/pages/FormatPage.svelte
@@ -0,0 +1,28 @@
+<script lang="typescript">
+	import {
+		AwafudgeParser,
+		AwafudgeFormatter,
+	} from "awafudge";
+
+	let input = $state('');
+	let output = $state('');
+
+	function formatAwafudge() {
+		const parser = new AwafudgeParser();
+		const ast = parser.parse(input);
+		const formatted = AwafudgeFormatter.format(ast);
+		output = formatted;
+	}
+</script>
+
+<div class="actions">
+  <button onclick={formatAwafudge}>Format</button>
+</div>
+
+<div class="input">
+  <textarea bind:value={input} rows={4} cols={80}></textarea>
+</div>
+
+<div class="output">
+  <textarea bind:value={output} readonly rows={4} cols={80}></textarea>
+</div>
