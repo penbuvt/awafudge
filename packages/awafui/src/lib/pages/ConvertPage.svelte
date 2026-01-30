bdc4acd38cd514a9efad40baf9a1322bca50d4ef
Penbu <penbu@penbuvt.ca>
2026-01-29 22:14:58 -0500

    Add structure to ConvertPage template
    Separate elements into actions, input and output.

--- packages/awafui/src/lib/pages/ConvertPage.svelte
+++ packages/awafui/src/lib/pages/ConvertPage.svelte
@@ -24,8 +24,15 @@
 	}
 </script>
 
-<button onclick={transpileToBrainfuck}>To brainfuck</button>
-<button onclick={transpileToAwafudge}>To awafudge</button>
-<textarea bind:value={input}></textarea>
-<textarea bind:value={output}></textarea>
+<div class="actions">
+  <button onclick={transpileToBrainfuck}>To brainfuck</button>
+  <button onclick={transpileToAwafudge}>To awafudge</button>
+</div>
 
+<div class="input">
+  <textarea bind:value={input} rows={4} cols={80}></textarea>
+</div>
+
+<div class="output">
+  <textarea bind:value={output} readonly rows={4} cols={80}></textarea>
+</div>
