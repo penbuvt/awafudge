8c2b3b4f91796fe2cd99b1c606780b01d1bacdba
Penbu <penbu@penbuvt.ca>
2026-01-29 20:39:45 -0500

    Extract existing contents into Converter page component
    There will be multiple pages. This is one of them.

---
+++ packages/awafui/src/lib/pages/Converter.svelte
@@ -0,0 +1,31 @@
+<script lang="typescript">
+	import {
+		AwafudgeParser,
+		AwafudgeFormatter,
+		BrainfuckParser,
+		BrainfuckFormatter,
+	} from "awafudge";
+
+	let input = $state('');
+	let output = $state('');
+
+	function transpileToBrainfuck() {
+		const parser = new AwafudgeParser();
+		const ast = parser.parse(input);
+		const formatted = BrainfuckFormatter.format(ast);
+		output = formatted;
+	}
+
+	function transpileToAwafudge() {
+		const parser = new BrainfuckParser();
+		const ast = parser.parse(input);
+		const formatted = AwafudgeFormatter.format(ast);
+		output = formatted;
+	}
+</script>
+
+<button onclick={transpileToBrainfuck}>To brainfuck</button>
+<button onclick={transpileToAwafudge}>To awafudge</button>
+<textarea bind:value={input}></textarea>
+<textarea bind:value={output}></textarea>
+
--- packages/awafui/src/routes/+page.svelte
+++ packages/awafui/src/routes/+page.svelte
@@ -1,32 +1,7 @@
 <script lang="typescript">
-	import {
-		AwafudgeParser,
-		AwafudgeFormatter,
-		BrainfuckParser,
-		BrainfuckFormatter,
-	} from "awafudge";
-
-	let input = $state('');
-	let output = $state('');
-
-	function transpileToBrainfuck() {
-		const parser = new AwafudgeParser();
-		const ast = parser.parse(input);
-		const formatted = BrainfuckFormatter.format(ast);
-		output = formatted;
-	}
-
-	function transpileToAwafudge() {
-		const parser = new BrainfuckParser();
-		const ast = parser.parse(input);
-		const formatted = AwafudgeFormatter.format(ast);
-		output = formatted;
-	}
+	import Converter from "$lib/pages/Converter.svelte";
 </script>
 
 <h1>awafudge</h1>
 
-<button onclick={transpileToBrainfuck}>To brainfuck</button>
-<button onclick={transpileToAwafudge}>To awafudge</button>
-<textarea bind:value={input}></textarea>
-<textarea bind:value={output}></textarea>
+<Converter />
