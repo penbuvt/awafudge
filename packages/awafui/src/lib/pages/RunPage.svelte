72ba83cd6387b083852f15888d50f44400d162f0
Penbu <penbu@penbuvt.ca>
2026-07-30 22:55:59 -0400

    Convert tab indents to spaces
    Thanks, Vim

--- packages/awafui/src/lib/pages/RunPage.svelte
+++ packages/awafui/src/lib/pages/RunPage.svelte
@@ -1,34 +1,34 @@
 <script lang="typescript">
-	import { tick } from "svelte";
-	import { AwafudgeParser, Interpreter } from "awafudge";
-	import MemoryView from "$lib/MemoryView.svelte";
+  import { tick } from "svelte";
+  import { AwafudgeParser, Interpreter } from "awafudge";
+  import MemoryView from "$lib/MemoryView.svelte";
 
-	let input = $state('');
-	let output = $state('');
-	let interpreter;
-	let runner;
+  let input = $state('');
+  let output = $state('');
+  let interpreter;
+  let runner;
 
   async function run() {
-		const parser = new AwafudgeParser();
-		const ast = parser.parse(input);
+    const parser = new AwafudgeParser();
+    const ast = parser.parse(input);
 
-		interpreter = new Interpreter();
+    interpreter = new Interpreter();
 
-		runner = interpreter.run(ast);
+    runner = interpreter.run(ast);
 
-		let step;
-		do {
-			step = runner.next();
-			if (!step.done) {
-				output = step.value;
-			}
-			await new Promise((resolve) => setTimeout(resolve, 300));
-		} while (!step.done);
+    let step;
+    do {
+      step = runner.next();
+      if (!step.done) {
+        output = step.value;
+      }
+      await new Promise((resolve) => setTimeout(resolve, 300));
+    } while (!step.done);
   }
 
-	function stop() {
-		runner?.return();
-	}
+  function stop() {
+    runner?.return();
+  }
 </script>
 
 <div class="actions">
